30ecc25a7bfc4160a69079926fbab0cd8cbeb878
hongbaoshi <hongbaoshi@xr.com>
2020-07-20 18:38:18 +0800

    fix

--- app/services/youtube_service.rb
+++ app/services/youtube_service.rb
@@ -3,8 +3,8 @@ class YoutubeService
     agent = Mechanize.new
     cc1 = Video.find_or_create_by(title: "生命科学", parent_id: 0)
     resp = agent.get("https://www.youtube.com/channel/UCCbYWuRvD2q_3qSla1gNHtg/videos").search("li.subnav_cat dl.sub_dl")
-    resp.each do |e1      catalog_name2 = e1.search|
-    (".sub_dt a").first.children.first.text.strip
+    resp.each do |e1|
+      catalog_name2 = e1.search(".sub_dt a").first.children.first.text.strip
     end
   end
 
