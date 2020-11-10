ab0ba24cd3c4cb9303ff270274954e2eb5fb8025
jamst <jamstjamst@163.com>
2020-11-10 11:18:45 +0800

    fix

--- db/migrate/20201107091242_create_medial_caches.rb
+++ db/migrate/20201107091242_create_medial_caches.rb
@@ -4,7 +4,7 @@ class CreateMedialCaches < ActiveRecord::Migration[5.2]
       t.string :uuid, comment: "uuid"
       t.string :title, comment: "名称"
       t.string :link, comment: "链接"
-      t.text :image_base64, default: "", comment: "图片内容"
+      t.text :image_base64, comment: "图片内容"
       t.string :medial_source, default: "youtube", comment: "媒体资源"
       t.string :local_model, default: "Video", comment: "资源类型"
       t.string :local_id, default: 0, comment: "本地缓存ID"
