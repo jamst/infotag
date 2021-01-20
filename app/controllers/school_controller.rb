6b975f9eb506e0773b38c86f8a14a8742a7a4835
jamst <jamstjamst@163.com>
2021-01-20 15:59:27 +0800

    school资讯流更新】

--- app/controllers/school_controller.rb
+++ app/controllers/school_controller.rb
@@ -10,8 +10,62 @@ class SchoolController < ActionController::Base
    Rails.logger.tagged("高校资讯流接口请求取值耗时") { Rails.logger.info("#{controller_name}-#{action_name}:#{start_time_bt}ms") }
    Rails.logger.tagged("高校资讯流接口请求取值耗时超时") { Rails.logger.info("#{controller_name}-#{action_name}:#{start_time_bt}ms") } if start_time_bt > 1000
   end
-  
+
+
   def index
+    page = params[:page].to_i
+    @info_forces = []
+    @video_tops = []
+    @info_tops = []
+
+    category_id = params[:category_id]
+
+    merge_videos = Video.classification_list(category_id)
+    @videos = Video.where(id:merge_videos)
+
+    merge_infos = Info.classification_list(category_id)
+    @infos = Info.where(id:merge_infos)
+
+    if page == 1 && params[:category_id].to_i == 7
+      info_force_ids = Info.get_force(params[:user_id].to_i)
+      @info_forces = Info.where(id:info_force_ids)
+
+      video_top_ids = Video.get_location(params[:user_id].to_i)
+      @video_tops = Video.where(id:video_top_ids)
+    end
+
+    flow_medias = {}
+    flow_medias[:tops] = []
+    flow_medias[:videos] = []
+    flow_medias[:infos] = []
+    # 推荐置顶
+    @video_tops.each do |_|
+      flow_medias[:tops] << {is_location_source: _.is_location_source, location_source_url: _.location_source_url ,author:_.author,medial_type: "video", medial_id:_.id, title:_.title,url:_.url,mobile_url:_.mobile_url,local_image_url:_.local_image_url,tag_ids:_.tag_ids.join(","), web_target:_.spider_target&.name, web_target_logo: _.spider_target&.logo_url,overlay_time: _.overlay_time, play_count:_.play_count }
+    end
+    @info_tops.each do |_|
+      flow_medias[:tops] << {medial_type: "info", medial_id:_.id, title:_.title,url:_.url,mobile_url:_.mobile_url,local_image_url:_.local_image_url,tag_ids:_.tag_ids.join(","), web_target:_.spider_target&.name, web_target_logo: _.spider_target&.logo_url,release_at: _.release_at }
+    end
+    # 强推信息
+    @info_forces.each do |_|
+      flow_medias[:infos] << {medial_type: "info", medial_id:_.id, title:_.title,url:_.url,mobile_url:_.mobile_url,local_image_url:_.local_image_url,tag_ids:_.tag_ids.join(","), web_target:_.spider_target&.name, web_target_logo: _.spider_target&.logo_url,release_at: _.release_at }
+    end
+    # 视频
+    @videos.each do |_|
+      flow_medias[:videos] << {author:_.author,medial_type: "video", medial_id:_.id, title:_.title,url:_.url,mobile_url:_.mobile_url,local_image_url:_.local_image_url,tag_ids:_.tag_ids.join(","), web_target:_.spider_target&.name, web_target_logo: _.spider_target&.logo_url,overlay_time: _.overlay_time, play_count:_.play_count }
+    end
+    # 资讯
+    @infos.each do |_|
+      flow_medias[:infos] << {medial_type: "info", medial_id:_.id, title:_.title,url:_.url,mobile_url:_.mobile_url,local_image_url:_.local_image_url,tag_ids:_.tag_ids.join(","), web_target:_.spider_target&.name, web_target_logo: _.spider_target&.logo_url,release_at: _.release_at }
+    end
+    
+    flow_medias[:infos] = flow_medias[:infos].sample(10)
+
+    flow_medias[:medias] = (flow_medias[:videos] + flow_medias[:infos]).sample(100)
+
+    render json: flow_medias  and return
+  end
+  
+  def index_school
     page = params[:page].to_i
     @info_forces = []
     @video_tops = []
@@ -45,10 +99,8 @@ class SchoolController < ActionController::Base
         merge_videos = $redis.smembers("videos_current")
       end
       
-      # @videos = Video.where(id:merge_videos).where("category_id != 1 and category_id != 7").sample(10)
-      # @infos = Info.where(id:merge_infos).where("category_id != 1 and category_id != 7").sample(10)
-      @videos = Video.where(id:merge_videos).where("category_id = 8").sample(10)
-      @infos = Info.where(id:merge_infos).where("category_id = 8").sample(10)
+      @videos = Video.where(id:merge_videos).where("category_id != 1 and category_id != 7").sample(10)
+      @infos = Info.where(id:merge_infos).where("category_id != 1 and category_id != 7").sample(10)
     end
 
     flow_medias = {}
