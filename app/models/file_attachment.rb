c67e8694158a9a4633f3d3a8b3c4fa23258a2c9c
jamst <jamstjamst@163.com>
2021-01-25 16:50:07 +0800

    添加cdn页面

--- app/models/file_attachment.rb
+++ app/models/file_attachment.rb
@@ -42,7 +42,8 @@ class FileAttachment < ApplicationRecord
   # 获取web访问地址
   def get_file_path
     mongo_config = CONFIG.mongo.to_hash
-    "#{mongo_config[:download_host]}/#{mongo_config[:database]}/#{self.path}"
+    # "#{mongo_config[:download_host]}/#{mongo_config[:database]}/#{self.path}"
+    "https://sz6.je2ci9.com/infoflow_pics/#{mongo_config[:database]}/#{self.path}"
   end
 
   # 直接mongodb获取文件流
