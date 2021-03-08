7353ac7208ef93feacb7dac35280139385830802
jamst <jamstjamst@163.com>
2021-03-08 17:54:34 +0800

    i8

--- app/models/recommend.rb
+++ app/models/recommend.rb
@@ -1,6 +1,6 @@
 class Recommend < ApplicationRecord
   belongs_to :mark
-  enum recomment_type: { top: 0, synthesize: 1 }
+  enum recommend_type: { top: 0, synthesize: 1 }
   default_scope -> {where(is_delete: 0)}
   enum status: { disabled: -1, enabled: 0 }
 end
